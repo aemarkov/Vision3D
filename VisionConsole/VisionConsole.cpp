055c41c806c86e1fd8d207b573b563a4166996a2
Markov <markovalex95@gmail.com>
2015-07-10 23:04:32 +0300

    INITIAL COMMIT Создана основная структура проекта:  - Vision3D - решение      - VisionCore - проект с основной логикой      - VisionConsole - проект, который использует        VisionCore и используется для отладки
    Добавлены ссылки на OpenCV в обоих проекта
    !!! ВНИМАНИЕ !!!
    Переменная окружения OPENCV_DIR должна быть настроена
    OPENCV_DIR = ...\<your opencv dir>\build\<your platform>\<your vc++ version>
    
    Проект VisionConsole связан с VisionCore
    
    Созданы классы:
     - StereoVision
     - StereoCalibData
     - IPointCloudStorage

---
+++ VisionConsole/VisionConsole.cpp
@@ -0,0 +1,20 @@
+/*///////////////////////////////////////////////////////////////////////////////////////
+    Это консольное приложение, использующее VisionCore.                                //
+    Оно используется для разработки и отладки системы компьютерного зрения.            //
+*////////////////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include "StereoVision.h"
+
+using namespace cv;
+
+int main(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
