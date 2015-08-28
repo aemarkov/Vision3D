c3d4120a954e608d228ee10e7c12b69cb907e7ba
Markov <markovalex95@gmail.com>
2015-08-28 11:48:18 +0300

    Добавил зависимости от PCL (include dir, lib dir, lib input)

--- VisionConsole/VisionConsole.cpp
+++ VisionConsole/VisionConsole.cpp
@@ -18,6 +18,23 @@
 
 #include "GlutViewer.h"
 
+#include <pcl/io/ply_io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/progressive_morphological_filter.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
+#include <pcl/visualization/cloud_viewer.h>
+
 using namespace cv;
 using namespace std;
 
